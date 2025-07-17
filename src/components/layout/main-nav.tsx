65c77dbd0a4193023f1529266389a2b278c41595
Foz3R <pawel.knot.1@gmail.com>
2025-07-17 13:42:14 +0200

    feat: add MainNav and MobileNav components for social links navigation

---
+++ src/components/layout/main-nav.tsx
@@ -0,0 +1,45 @@
+import Link from "next/link";
+import { env } from "~/env";
+
+import { Discord, Instagram, Youtube } from "../icons";
+import PixelMesh from "../ui/pixel-mesh";
+
+export default function MainNav() {
+  return (
+    <nav className="relative hidden h-9 items-center text-sm text-white sm:flex">
+      <Link
+        href={env.NEXT_PUBLIC_INSTAGRAM_URL}
+        className="group relative h-full px-3 py-1.5"
+      >
+        <Instagram className="size-6 drop-shadow-[0_0_1px_#000,_0_0_2px_rgba(255,255,255,0.75)]" />
+        <PixelMesh
+          columns={4}
+          className="transition-all group-hover:bg-pink-600/35"
+          glass
+        />
+      </Link>
+      <Link
+        href={env.NEXT_PUBLIC_YOUTUBE_URL}
+        className="group relative h-full px-3 py-1.5"
+      >
+        <Youtube className="size-6 drop-shadow-[0_0_1px_#000,_0_0_2px_rgba(255,255,255,0.75)]" />
+        <PixelMesh
+          columns={4}
+          className="transition-all group-hover:bg-red-600/35"
+          glass
+        />
+      </Link>
+      <Link
+        href={env.NEXT_PUBLIC_DISCORD_URL}
+        className="group relative h-full px-3 py-1.5"
+      >
+        <Discord className="size-6 drop-shadow-[0_0_1px_#000,_0_0_2px_rgba(255,255,255,0.75)]" />
+        <PixelMesh
+          columns={4}
+          className="transition-all group-hover:bg-indigo-600/35"
+          glass
+        />
+      </Link>
+    </nav>
+  );
+}
--- src/components/layout/site-header.tsx
+++ src/components/layout/site-header.tsx
@@ -1,60 +1,28 @@
 import Link from "next/link";
-import { env } from "~/env";
 
+import { Logo } from "../icons";
 import PixelMesh from "../ui/pixel-mesh";
-import { Discord, Instagram, Logo, Youtube } from "../icons";
+import MainNav from "./main-nav";
+import MobileNav from "./mobile-nav";
 
 export default async function SiteHeader() {
   return (
     <header id="nav" className="sticky top-0 z-50 w-full fill-white text-white">
-      <div className="container mx-auto flex h-16 w-full items-start justify-center gap-10 px-5 sm:items-center sm:justify-between">
-        <div className="flex flex-col items-center justify-center">
-          <Link
-            href="/"
-            className="relative h-9 w-[17.25rem] shrink-0 scroll-smooth px-3 text-center"
-          >
-            <Logo className="w-full fill-white py-2 drop-shadow-[0_0_1px_#000,_0_0_2px_rgba(255,255,255,0.75)]" />
-            <PixelMesh columns={23} glass />
-          </Link>
+      <div className="container mx-auto flex h-16 w-full flex-col items-center justify-center px-5 sm:flex-row sm:justify-between sm:gap-10">
+        <Link
+          href="/"
+          className="group relative flex h-9 w-[18rem] shrink-0 items-center px-3 text-center"
+        >
+          <Logo className="w-full fill-white drop-shadow-[0_0_1px_#000,_0_0_2px_rgba(255,255,255,0.75)]" />
+          <PixelMesh
+            columns={24}
+            className="group-hover:bg-neon-yellow/25 transition-all"
+            glass
+          />
+        </Link>
 
-          <nav className="relative flex h-9 items-center text-sm text-white sm:hidden">
-            <Link
-              href={env.NEXT_PUBLIC_INSTAGRAM_URL}
-              className="h-full px-3 py-1.5"
-            >
-              <Instagram className="size-6 drop-shadow-[0_0_1px_#000,_0_0_2px_rgba(255,255,255,0.75)]" />
-            </Link>
-            <Link
-              href={env.NEXT_PUBLIC_YOUTUBE_URL}
-              className="h-full px-3 py-1.5"
-            >
-              <Youtube className="size-6 drop-shadow-[0_0_1px_#000,_0_0_2px_rgba(255,255,255,0.75)]" />
-            </Link>
-            <Link
-              href={env.NEXT_PUBLIC_DISCORD_URL}
-              className="h-full px-3 py-1.5"
-            >
-              <Discord className="size-6 drop-shadow-[0_0_1px_#000,_0_0_2px_rgba(255,255,255,0.75)]" />
-            </Link>
-
-            <PixelMesh columns={12} glass />
-          </nav>
-        </div>
-
-        {/* Main Navigation */}
-        <nav className="relative hidden h-9 items-center text-sm text-white sm:flex">
-          <Link href="/" className="h-full px-3 py-1.5">
-            <Instagram className="size-6 drop-shadow-[0_0_1px_#000,_0_0_2px_rgba(255,255,255,0.75)]" />
-          </Link>
-          <Link href="/" className="h-full px-3 py-1.5">
-            <Youtube className="size-6 drop-shadow-[0_0_1px_#000,_0_0_2px_rgba(255,255,255,0.75)]" />
-          </Link>
-          <Link href="/" className="h-full px-3 py-1.5">
-            <Discord className="size-6 drop-shadow-[0_0_1px_#000,_0_0_2px_rgba(255,255,255,0.75)]" />
-          </Link>
-
-          <PixelMesh columns={12} glass />
-        </nav>
+        <MainNav />
+        <MobileNav />
       </div>
     </header>
   );
